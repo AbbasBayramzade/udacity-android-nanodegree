0d15ed3fc7a728d6c4e53025e3eddb5f62c61050
Matanat Ahmadova <ahmadova.matanat01@gmail.com>
2018-02-15 13:04:50 +0100

    Update DetailActivity to display required data. Implement Json Parser. Update UI of DetailActivity

--- sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -2,13 +2,22 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,7 +29,10 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +55,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +68,50 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView originTv = findViewById(R.id.origin_tv);
+        originTv.setText(sandwich.getPlaceOfOrigin());
 
+        // todo empty strings' visibility
+
+
+        TextView alsoKnowTv = findViewById(R.id.also_known_tv);
+        TextView alsoKnowLabel = findViewById(R.id.also_known_label);
+        String[] alsoKnowArray = sandwich.getAlsoKnownAs().toArray(new String[0]);
+        if (alsoKnowArray.length == 0){
+            alsoKnowTv.setVisibility(View.GONE);
+            alsoKnowLabel.setVisibility(View.GONE);
+        }else{
+            alsoKnowTv.setVisibility(View.VISIBLE);
+            alsoKnowLabel.setVisibility(View.VISIBLE);
+            alsoKnowTv.setText(Arrays.toString(alsoKnowArray));
+        }
+
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        descriptionTv.setText(sandwich.getDescription());
+
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+        TextView ingredientsLabel = findViewById(R.id.ingredients_label);
+        // get ingredients list and convert it to string array
+        String[] ingredientsArray = sandwich.getIngredients().toArray(new String[0]);
+        if(ingredientsArray.length == 0){
+            ingredientsTv.setVisibility(View.GONE);
+            ingredientsLabel.setVisibility(View.GONE);
+        } else{
+            ingredientsTv.setVisibility(View.VISIBLE);
+            ingredientsLabel.setVisibility(View.VISIBLE);
+            //todo set text
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich-club-starter-code-master/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,61 @@
 package com.udacity.sandwichclub.utils;
 
+import android.text.TextUtils;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+
+        if(TextUtils.isEmpty(json))
+            return null;
+
+        try {
+            // get jsonobject from input string
+            JSONObject jsonObject = new JSONObject(json);
+
+            // get 'mainName'string content from the jsonobject 'name'
+            String mainName = jsonObject.getJSONObject("name").getString("mainName");
+
+            // get jsonarray "alsoKnownAs"
+            JSONArray jsonArray = jsonObject.getJSONObject("name").
+                                    getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<>();
+            // check if array isn't empty
+            if (jsonArray.length() > 0){
+                for (int i = 0; i < jsonArray.length(); i++)
+                    alsoKnownAs.add(jsonArray.getString(i));
+            }
+
+            // get 'placeOfOrigin' string
+            String placeOfOrigin = jsonObject.getString("placeOfOrigin");
+            //get 'description' string
+            String description = jsonObject.getString("description");
+            // get 'image' string
+            String image = jsonObject.getString("image");
+
+            // get jsonarray 'ingredients'
+            JSONArray ingredientsJsonArray = jsonObject.getJSONArray("ingredients");
+            List<String> ingredients = new ArrayList<>();
+            if(ingredientsJsonArray.length() > 0){
+                for (int i = 0; i < jsonArray.length(); i++)
+                    ingredients.add(ingredientsJsonArray.getString(i));
+            }
+
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
