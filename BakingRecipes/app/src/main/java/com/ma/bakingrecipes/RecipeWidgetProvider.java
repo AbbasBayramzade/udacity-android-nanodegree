dbc5fc4c861eb9e4c0f10c6f549d7023e3a24fab
amatanat <ahmadova.matanat01@gmail.com>
2018-04-17 21:47:55 +0200

    Update the code for the widget. Implement widget click, open MainActivity.

--- BakingRecipes/app/src/main/java/com/ma/bakingrecipes/RecipeWidgetProvider.java
+++ BakingRecipes/app/src/main/java/com/ma/bakingrecipes/RecipeWidgetProvider.java
@@ -1,9 +1,13 @@
 package com.ma.bakingrecipes;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.ma.bakingrecipes.ui.list.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -13,10 +17,14 @@ public class RecipeWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // create pending intent to open MainActivity on widget click
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
