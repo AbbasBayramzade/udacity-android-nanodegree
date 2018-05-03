9fa42a71c4a6e250a55cf65dbaca6ac38080ccaa
amatanat <ahmadova.matanat01@gmail.com>
2018-05-03 12:11:05 +0200

    refactor: update DetailActivity.
    Implement button click.

--- capstoneProject/TravelDroid/app/src/main/java/com/ma/traveldroid/ui/DetailActivity.java
+++ capstoneProject/TravelDroid/app/src/main/java/com/ma/traveldroid/ui/DetailActivity.java
@@ -1,13 +1,62 @@
 package com.ma.traveldroid.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ma.traveldroid.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.country_name_textview)
+    AutoCompleteTextView mCountryName;
+    @BindView(R.id.visited_period_textview)
+    EditText mVisitedPeriod;
+    @BindView(R.id.save_button)
+    Button mSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        ButterKnife.bind(this);
+
+        setupAutoCompleteTextView();
+
+        buttonClick();
+
+    }
+
+    private void buttonClick() {
+        mSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO save country name and visited period in database
+
+                Intent intent = new Intent(DetailActivity.this, MainActivity.class);
+                intent.putExtra("COUNTRY", mCountryName.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setupAutoCompleteTextView() {
+        // Get the string array
+        String[] countries = getResources().getStringArray(R.array.countries_array);
+
+        // Create the adapter and set it to the AutoCompleteTextView
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        mCountryName.setAdapter(adapter);
     }
 }
