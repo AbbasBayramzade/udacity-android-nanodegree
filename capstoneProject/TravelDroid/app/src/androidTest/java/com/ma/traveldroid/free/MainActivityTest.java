fc80e20cdd1165dd1b647c6dc84753948d778d24
amatanat <ahmadova.matanat01@gmail.com>
2018-05-15 18:26:52 +0200

    test: implement MainActivityTest.

---
+++ capstoneProject/TravelDroid/app/src/androidTest/java/com/ma/traveldroid/free/MainActivityTest.java
@@ -0,0 +1,37 @@
+package com.ma.traveldroid.free;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
+
+import com.ma.traveldroid.R;
+import com.ma.traveldroid.ui.main.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void clickRecyclerViewItem_opensDetailActivity(){
+        onView(ViewMatchers.withId(R.id.fab))
+                .perform(click());
+
+        onView(withId(R.id.country_name_label))
+                .check(matches(withText("Country name")));
+    }
+}
